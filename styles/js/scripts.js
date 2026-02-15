9467edf39e451a3be1b0f98ed0df47238767ecb1
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-02-16 02:46:46 +0600

    Java scripts and Dom

---
+++ styles/js/scripts.js
@@ -0,0 +1,6 @@
+const liElements = document.getElementsByTagName("li");
+console.log(liElements);
+
+const catchElement = document.getElementById("catch");
+console.log(catchElement.innerText);
+// catchElement.innerText = "parti nah";
