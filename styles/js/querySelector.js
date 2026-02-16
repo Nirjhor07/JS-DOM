8e6e639fc68035fb59c548be4df84a3e47adabbb
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-02-16 19:35:31 +0600

    Dom Practice day - 2

---
+++ styles/js/querySelector.js
@@ -0,0 +1,27 @@
+//qurey selector all
+const sections = document.querySelectorAll("section");
+console.log(sections);
+// for (const section of sections) {
+//   console.log(section);
+//   //adding styles to the section
+//   section.style.backgroundColor = "lightgreen";
+//   section.style.padding = "20px";
+//   section.style.marginBottom = "18px";
+//   section.style.fontSize = "1.25rem";
+//   //   section.style.fontFamily = 'timesNewRoman'
+//   //   section.style.animation = "toggle 0.5s ease-in-out";
+//   section.style.borderRadius = "15px";
+//   section.style.textAlign = "justify";
+// }
+
+for (const section of sections) {
+  section.classList.add("styles");
+}
+
+const specifcSection = document.querySelector("#hudai");
+console.log(specifcSection);
+if (specifcSection.classList.contains("styles")) {
+  specifcSection.classList.toggle("styles");
+} else {
+  console.log("do nothing");
+}
